ddd9a4584ef9a4ab48b7fff921f65276c5a79de4
abhay <abhayforwork02@gmail.com>
2024-12-04 23:56:34 +0530

    streams backend

---
+++ app/api/streams/next/route.ts
@@ -0,0 +1,60 @@
+import { prismaClient } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const session = await getServerSession();
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? "",
+    },
+  });
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "Unauthenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const mostUpvotedStream = await prismaClient.stream.findFirst({
+    where: {
+      userId: user.id,
+      played: false,
+    },
+    orderBy: {
+      upvotes: {
+        _count: "desc",
+      },
+    },
+  });
+  await Promise.all([
+    prismaClient.currentStream.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        userId: user.id,
+        streamId: mostUpvotedStream?.id,
+      },
+      create: {
+        userId: user.id,
+        streamId: mostUpvotedStream?.id,
+      },
+    }),
+    prismaClient.stream.update({
+      where: {
+        id: mostUpvotedStream?.id ?? "",
+      },
+      data: {
+        played: true,
+        playedTs: new Date(),
+      },
+    }),
+  ]);
+  return NextResponse.json({
+    stream: mostUpvotedStream,
+  });
+}
