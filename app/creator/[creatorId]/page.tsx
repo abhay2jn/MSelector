03baddfd72eb873ecf933384be92005e8b7bfc58
abhay <abhayforwork02@gmail.com>
2025-01-01 00:01:51 +0530

    redirect to homepage

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,12 +1,19 @@
 import StreamView from "@/components/StreamView";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Creator ({
-   params: { creatorId },
+export default async function Creator({
+  params: { creatorId },
 }: {
   params: {
     creatorId: string;
   };
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user.id) redirect("/");
+
   return (
     <div>
       <StreamView creatorId={creatorId} playVideo={false} />
