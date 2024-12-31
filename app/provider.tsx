f0cd76bcac69dd1c4504e851fb0580b840a35583
abhay <abhayforwork02@gmail.com>
2024-12-31 23:52:20 +0530

    Provider

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
